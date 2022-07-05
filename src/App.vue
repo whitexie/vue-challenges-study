8adb97297201a5349fea95bef4d02545652ea8a9
Ysansan <shieber@163.com>
2022-07-05 13:37:35 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="container">
+    <aside></aside>
+    <RouterView />
+  </div>
+</template>
+
+<style>
+</style>
